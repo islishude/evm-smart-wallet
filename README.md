084437499e3ae9c51ecfd0e6d616a9d7485fefab
Shude Li <islishude@gmail.com>
2020-12-28 22:42:04 +0800

    update doc and tests (#3)

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-The Ethereum wallet contract for exchange
+The Ethereum wallet contract for centralized crypto-currency exchange 
+
+[![Test](https://github.com/islishude/eth-create2-wallet/workflows/test/badge.svg?branch=main)]()
+
+# Features
+
+1. No ether dust for collecting ERC20 tokens
+2. Better way to check internal ether deposit
+
+# Why and How?
+
+on centralized cryptocurrency exchanges, each user has their own address to top up their Ether or ERC20 tokens(e.g. USDT).
+
+if a user just tops up ERC20 tokens, but not ETH,if the wallet wants to collect these tokens into one address, it needs to first transfer some ETH as miner fee.
+
+but the ETH of the cost is estimated and may be more than what is actually needed.
+
+in the long run, the address of users will have many exceptionally small dust ETH.
+
+this program solves this unavoidable problems.
+
+the deposit addresses are not created by the private key but by [CREATE2](https://eips.ethereum.org/EIPS/eip-1014),and the address with permissions could call function to transfer tokens.
+
+so the operation of paying the fee is diverted to other address.
+
+You can find out more by reading [this blog](https://github.com/islishude/blog/issues/221).
--- contracts/controller.sol
+++ contracts/controller.sol
@@ -23,6 +23,10 @@ contract Controller {
     owner = msg.sender;
   }
 
+  /**
+   * @notice create new replica addresses
+   * @param salts the salt list for create2
+   */
   function create(bytes32[] calldata salts) external OnlyOwner {
     for (uint256 i = 0; i < salts.length; i++) {
       Replica created = new Replica{ salt: salts[i] }();
@@ -31,6 +35,10 @@ contract Controller {
     }
   }
 
+  /**
+   * @notice collect ethers
+   * @param targets the replica list to transfer ethers
+   */
   function flushEther(Replica[] calldata targets) external OnlyOwner {
     for (uint256 i = 0; i < targets.length; i++) {
       Replica target = targets[i];
@@ -40,10 +48,10 @@ contract Controller {
   }
 
   /**
-   * @notice flush ERC20 Token
-   * @param token 代币地址
-   * @param targets 需要转移代币余额的目标地址
-   * @param checkres 是否验证 ERC20.transfer() 返回值，通常情况下需要验证，但如果代币合约提供了返回值，但始终返回 false 特殊合约则不能验证
+   * @notice collect ERC20 tokens
+   * @param token the ERC20 token address
+   * @param targets the replica list to collect
+   * @param checkres verify return of ERC20.transfer() or not,you should give true unless token.transfer() always return false
    */
   function flushERC20Token(
     address token,
@@ -66,6 +74,12 @@ contract Controller {
     }
   }
 
+  /**
+   * @notice call any for a replica address
+   * @param token the address to call by replica
+   * @param target the replica address
+   * @param params abi encoded call data
+   */
   function dispatch(
     address token,
     Replica target,
