98793a60b1474036e5dd614cc1630c87567ae749
islishude <islishude@gmail.com>
2021-05-23 19:30:17 +0800

    update

---
+++ contracts/Replica.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "./interfaces/IReplica.sol";
+import "./interfaces/IController.sol";
+
+contract Replica is IReplica {
+    address public controller;
+
+    function initial(address _controller) external override {
+        require(controller == address(0));
+        controller = _controller;
+    }
+
+    function dispatch(
+        address target,
+        uint256 value,
+        bytes calldata input
+    ) external override returns (bytes memory) {
+        require(msg.sender == IController(controller).proxy(), "403");
+        (bool success, bytes memory data) = target.call{value: value}(input);
+        require(success, "dispach failed");
+        return data;
+    }
+
+    receive() external payable {}
+}
